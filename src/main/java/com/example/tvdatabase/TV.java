8d10f08a5b6242a790d588a79bf3831b1e9c26a4
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-05-03 09:06:13 -0400

    made a plan ol' TV object

---
+++ src/main/java/com/example/tvdatabase/TV.java
@@ -0,0 +1,28 @@
+package com.example.tvdatabase;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TV {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	@SuppressWarnings("unused")
+	private String model;
+	@SuppressWarnings("unused")
+	private String dateUpdated;
+	@SuppressWarnings("unused")
+	private String price;
+
+	protected TV() {
+	}
+
+	public TV(String model, String dateUpdated, String price) {
+		this.model = model;
+		this.dateUpdated = dateUpdated;
+		this.price = price;
+	}
+}
