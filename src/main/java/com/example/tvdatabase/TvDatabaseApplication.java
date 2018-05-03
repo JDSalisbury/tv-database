affcfe1a5ba895cf4c1cc55ab08c458614f1c50f
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-05-03 09:00:35 -0400

    First push 9:00

---
+++ src/main/java/com/example/tvdatabase/TvDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.tvdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvDatabaseApplication.class, args);
+	}
+}
